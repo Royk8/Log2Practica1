98a74bac57965eb5c58677f36e51597349a20d5d
Royk8 <roy.maestre@udea.edu.co>
2020-01-16 13:39:40 -0500

    Estructura completa del proyecto

--- src/Matriz.java
+++ src/Matriz.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -8,10 +9,14 @@ public class Matriz {
 
     public Matriz ()throws IOException {
         //Constructor de la matriz que lee los datos desde un archivo de texto
+
+        //Se usa para encontrar la direccion del archivo y almacenarla en la variable 'ruta'.
+        File archivo = new File(System.getProperty("user.dir"));
+        String ruta = archivo.getAbsolutePath()+"\\Matriz.txt";
+
         BufferedReader leerArchivo = null;
         try{
-            leerArchivo = new BufferedReader(new FileReader(
-                    "D:\\Roy\\Cosas Udea\\Java 2019-2\\Practicas Logica\\Log2Practica1\\Matriz.txt"));
+            leerArchivo = new BufferedReader(new FileReader(ruta));
             String linea;
             int numeroLinea = 0;
             int orden=0;
@@ -46,10 +51,93 @@ public class Matriz {
 
 
     public void mostrarMatriz(){
+        //Metodo que imprime los valores de la matriz
         System.out.println("La matriz de orden " + orden + " es la siguiente: ");
         for(int i = 1; i <= orden; i++){
             for(int j = 1; j<= orden; j ++) System.out.print(M[i][j]+" ");
             System.out.println("");
         }
+    }
+
+    public void sumaDatosMatriz(){
+        //Metodo que retorna un entero con la suma de los datos de la matriz
+        int suma = 0;
+        for(int i = 1; i <= orden; i++){
+            for (int j = 1; j <= orden; j++)
+                suma = suma + M[i][j];
+        }
+        System.out.println("La suma de todos los datos de la matriz es: " + suma);
+    }
+
+    public void mayorValor(){
+        //Metodo que imprime el mayor valor de la matriz y su posicion
+        int mayor = 0, iMayor = -1, jMayor = -1;
+        for (int i = 1; i <= orden; i++){
+            for(int j = 1; j <= orden; j++){
+                if (M[i][j] > mayor){
+                    mayor = M[i][j];
+                    iMayor = i;
+                    jMayor = j;
+                }
+            }
+        }
+        System.out.println("El mayor valor de la matriz es " + mayor +
+                " y se encuentra en la posicion [" + iMayor + "][" + jMayor + "]");
+    }
+
+    public void menorValor(){
+        //Metodo que imprime el menor valor de la matriz y su posicion
+        int menor = 0, iMenor = -1, jMenor = -1;
+        for (int i = 1; i <= orden; i++){
+            for(int j = 1; j <= orden; j++){
+                if (M[i][j] < menor){
+                    menor = M[i][j];
+                    iMenor = i;
+                    jMenor = j;
+                }
+            }
+        }
+        System.out.println("El menor valor de la matriz es " + menor +
+                " y se encuentra en la posicion [" + iMenor + "][" + jMenor + "]");
+    }
+
+    public void repeticionDatos(){
+
+    }
+
+    public void promedioTridiagonal(){
+
+    }
+
+    public void mayorDatoCadaFila(){
+
+    }
+
+    public void menorDatoCadaColumna(){
+
+    }
+
+    public void datoMayorSumaDigitosCadaFila(){
+
+    }
+
+    public void intercambiaUnaFilaConColumna(){
+
+    }
+
+    public void ordenarAscendentePorFilas(){
+
+    }
+
+    public void ordenarCadaFilaDescendente(){
+
+    }
+
+    public void ordenarDescendentePorFilaUno(){
+
+    }
+
+    public void ordenarAscendenteDigitosTridiagonalSegundaria(){
+
     }
 }
--- src/Practica1.java
+++ src/Practica1.java
@@ -24,13 +24,13 @@ public class Practica1 {
                     matriz.mostrarMatriz();
                     break;
                 case 2:
-                    menuOperacionesMatriz();
+                    menuOperacionesMatriz(matriz);
                     break;
                 case 3:
-                    menuOperacionesFilasColumnas();
+                    menuOperacionesFilasColumnas(matriz);
                     break;
                 case 4:
-                    menuOperacionesOrdenamiento();
+                    menuOperacionesOrdenamiento(matriz);
                     break;
                 case 0:
                     System.out.println("Adios!");
@@ -41,20 +41,94 @@ public class Practica1 {
 
     }
 
-    public void menuOperacionesMatriz(){
-        System.out.println("Seleccione la operacion que desea realizar.");
+    public void menuOperacionesMatriz(Matriz matriz){
+        int opcion = 0;
+        do {
+            System.out.println("Seleccione la operacion que desea realizar.");
+            System.out.println("1. Hallar la suma de los terminos de la matriz");
+            System.out.println("2. Hallar el mayor valor en la matriz");
+            System.out.println("3. Hallar el menor valor en la matriz");
+            System.out.println("4. Mostrar el numero de veces que se repite cada dato");
+            System.out.println("5. Mostrar el promedio de los datos de la tridiagonal principal");
+            System.out.println("0. Volver al menu principal");
+            opcion = leerInt(0,5);
+            switch (opcion){
+                case 1:
+                    matriz.sumaDatosMatriz();
+                    break;
+                case 2:
+                    matriz.mayorValor();
+                    break;
+                case 3:
+                    matriz.menorValor();
+                    break;
+                case 4:
+                    matriz.repeticionDatos();
+                    break;
+                case 5:
+                    matriz.promedioTridiagonal();
+                    break;
+            }
+        }while(opcion != 0);
     }
 
-    public void menuOperacionesFilasColumnas(){
-
+    public void menuOperacionesFilasColumnas(Matriz matriz){
+        int opcion = 0;
+        do {
+            System.out.println("Seleccione la operacion que desea realizar.");
+            System.out.println("1. Mostrar el mayor dato de cada fila");
+            System.out.println("2. Mostrar el menor dato de cada columna");
+            System.out.println("3. Mostrar el dato con la mayor suma de digitos en cada fila");
+            System.out.println("4. Intercambiar los datos de una fila con los de una columna");
+            System.out.println("0. Volver al menu principal");
+            opcion = leerInt(0,4);
+            switch (opcion){
+                case 1:
+                    matriz.mayorDatoCadaFila();
+                    break;
+                case 2:
+                    matriz.menorDatoCadaColumna();
+                    break;
+                case 3:
+                    matriz.datoMayorSumaDigitosCadaFila();
+                    break;
+                case 4:
+                    matriz.intercambiaUnaFilaConColumna();
+                    break;
+            }
+        }while(opcion != 0);
     }
 
-    public void menuOperacionesOrdenamiento(){
-
+    public void menuOperacionesOrdenamiento(Matriz matriz){
+        int opcion = 0;
+        do {
+            System.out.println("Seleccione la operacion que desea realizar.");
+            System.out.println("1. Ordenar la matriz ascendentemente por filas");
+            System.out.println("2. Ordenar cada fila descendentemente");
+            System.out.println("3. Ordenar descendentemente por fila 1?");
+            System.out.println("4. Ordenar ascendentemente los digitos de cada dato de la tridiagonal segundaria");
+            System.out.println("0. Volver al menu principal");
+            opcion = leerInt(0,4);
+            switch (opcion){
+                case 1:
+                    matriz.ordenarAscendentePorFilas();
+                    break;
+                case 2:
+                    matriz.ordenarCadaFilaDescendente();
+                    break;
+                case 3:
+                    matriz.ordenarDescendentePorFilaUno();
+                    break;
+                case 4:
+                    matriz.ordenarAscendenteDigitosTridiagonalSegundaria();
+                    break;
+            }
+        }while(opcion != 0);
     }
 
     public int leerInt(int min, int max){
-        int n = -1;
+        //Metodo para leer un Entero ingresado por el usuario, valido en un intervalo y con manejo de excepciones.
+        int n = min-1;
         while(true){
             try{
                 n = Integer.parseInt(leer.nextLine());
